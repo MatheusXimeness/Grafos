ed3ce5c6ed791c197d324cd19ac955275b0f2eae
matheusximeness <matheus.ximenes2016@outlook.com>
2021-04-11 21:01:12 -0300

    Finish

--- Trabalho1/A.cpp
+++ Trabalho1/A.cpp
@@ -7,7 +7,6 @@ int main(){
     // ------ DECLARANDO VARIÁVEIS ----- //
         int numTest;             // número de casos teste
         int VERTICES, ARESTAS;   // número de VÉRTICES e número de ARESTAS
-        vector<vector<bool>> MI; // matriz de incidência
         int booleanoDaMatriz;    // irá setar os valores da matriz
     // ----- ----- //
 
@@ -16,32 +15,46 @@ int main(){
         
         for( int i=0 ; i<numTest ;i++ ){
             cin >> VERTICES >> ARESTAS;
+            vector<vector<bool>> MI(VERTICES, vector<bool>(ARESTAS, false)); // inicializando uma matriz de incidencia setada com false
 
             for( int j = 0 ; j < VERTICES ; j++ ){
                 for( int k = 0 ; k < ARESTAS ; k++ ){
                     cin >> booleanoDaMatriz;
+
                     if(booleanoDaMatriz == 1){
-                        MI[i].push_back(true);
-                    } else {
-                        MI[i].push_back(false);   
-                    }     
+                        MI[j][k] = true;
+                    }
                 }
             }
             // ----- CONFERINDO SE DADA A MATRIZ POSSO OBTER UM GRAFO SIMPLES NAO DIRECIONADO ----- //
-                int contTrueInLines = 0;
-                
-                for( int i = 0 ; i < VERTICES ; i++ ){
-                    for ( int j = 0 ; j < ARESTAS ; j++ ){
-                        if(MI[i][j] == true) contTrueInLines++;
+                int contIguais = 0;  // conta a existencia de arestas diferentes incidindo em dois vertices
+                int cont1emCol = 0;  // conta a quantidade de 1's nas colunas
+                int contErrados = 0; // caso aconteça alguma falha registra o erro
+
+                for ( int y = 0 ; y < ARESTAS  ; y++ ){
+                    for(int z = 0 ; z < VERTICES ; z++){
+                        if(MI[z][y] == true) cont1emCol++;
                     }
-                    if(contTrueInLines < ARESTAS - 1) {
-                        contTrueInLines = 0;
-                    } else { 
-                        cout << "No" << endl;
-                        break;
+                    if(cont1emCol!=2) contErrados++;
+                    cont1emCol = 0;
+                }
+
+                for( int x = 0 ; x < ARESTAS - 1 ; x++ ){
+                    for ( int y = x + 1 ; y < ARESTAS  ; y++ ){
+                        for(int z = 0 ; z < VERTICES ; z++){
+                            if(MI[z][x] == MI[z][y] && MI[z][x] == true){
+                                contIguais++;
+                            }
+                        }
+                        if(contIguais >= 2){
+                            contErrados++;
+                        }
+                        contIguais = 0;
                     }
                 }
-                cout << "Yes" << endl;  
+            
+                if(contErrados>0) cout << "No" << "\n";
+                else cout << "Yes" << "\n";
             // ----- ----- //    
         }
     // ----- ----- //
