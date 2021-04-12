bc2bdd0130b45f2e2530c77e5d9a88007d23d4e5
matheusximeness <matheus.ximenes2016@outlook.com>
2021-04-11 22:53:50 -0300

    Finish

---
+++ Trabalho1/C.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<string>
+#include<set>
+using namespace std;
+
+void descobreTamFloresta(){
+	string st;        // auxilia na leitura da quantidade de arestas
+	string arestas;   // auxilia na leitura da quantidade de bolotas
+	string verts;     // auxilia na leitura da quantidade de vértices
+	int nArestas = 0; // guarda o número de arestas
+	int nCC;          // guarda o número de componentes conexos
+	// ----- LEITURA ----- //
+		while(true){
+			cin >> st;
+			arestas += st;
+			if(st[0] == '*') break;
+			nArestas++;
+		}
+		cin >> verts;
+	// -----  ----- //
+	set<char> vertices(verts.begin(), verts.end());
+	vertices.erase(','); // apaga a vírgula que separa as letras maiusculas
+
+	nCC = vertices.size() - nArestas; // pela teoria, temos que Número de componentes conexos é |V| - |E|
+
+	set<char> verticesEmArestas(arestas.begin(), arestas.end());
+	int numIsolados = 0;
+	for(char vert:vertices){
+		if(verticesEmArestas.count(vert) == 0) numIsolados++;
+	}
+	cout << "There are " << nCC-numIsolados  << " tree(s) and " << numIsolados << " acorn(s)." <<  "\n";
+}
+
+
+int main(int argc, char **argv){
+    int nTestes; // número de casos teste
+	cin >> nTestes;
+	for(int i = 0 ; i < nTestes ; i++){
+		descobreTamFloresta();
+	}
+    return 0;
+}
