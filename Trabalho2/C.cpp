1017c1391cda83f3f0478b6bfb85f66d44edc0c6
matheusximeness <matheus.ximenes2016@outlook.com>
2021-04-20 23:39:28 -0300

    Fixing

--- Trabalho2/C.cpp
+++ Trabalho2/C.cpp
@@ -1,27 +1,98 @@
 #include<iostream>
+#include<queue>
 #include<vector>
 using namespace std;
 
-void dfs(int R, int C, int i, int j, vector<vector<bool>> &visitadosPorJoe, vector<vector<char>> &Labirinto){
-    if(visitadosPorJoe[i][j] == true) return;
-    visitadosPorJoe[i][j] = true;
-    if(i+1 < R){
-        if(visitadosPorJoe[i+1][j] == false){
-            Labirinto[i+1][j] = 'J';
+void bfsFogo(int R, int C, vector<vector<char>> &Labirinto, vector<vector<int>> &visitadosPorFogo){
+    
+    queue<pair<int, int>> fila;
+    pair<int,int> coordenadas;
 
-
-
+    for( int i = 0 ; i < R ; i++ ) {
+        for ( int j = 0 ; j < C ; j++ ) {
+            visitadosPorFogo[i][j] = 0;
+            if ( Labirinto[i][j] == 'F' ) {
+                coordenadas = make_pair (i,j);
+                fila.push( coordenadas );
+            }
         }
     }
-    if(i-1 >= 0){}
-        
-    if(j+1 < C){}
-        
-    if(j-1 >= 0){}
     
+   
+    pair<int,int> vertice;
+    
+    while( !fila.empty() ){
+        vertice.first  = fila.front().first; 
+        vertice.second = fila.front().second; 
+        fila.pop( ); 
+
+        //visitaVertice() não sei se é necessário
+        // ----- Para todos os vizinhos ----- //    
+            if( vertice.first+1 < R ) {
+                if( visitadosPorFogo[ vertice.first+1 ][ vertice.second ] == 0 ) {
+                    if( Labirinto[ vertice.first+1 ][ vertice.second ] == '.' || Labirinto[ vertice.first+1 ][ vertice.second ]  == 'J' ) {
+                        visitadosPorFogo[ vertice.first+1 ][ vertice.second ]++;
+
+                        fila.push( make_pair( vertice.first+1 , vertice.second ) );
+                        cout << "i: " << vertice.first+1  << " j: " << vertice.second << endl;
+                    }
+                } else { //caso seja maior que 0
+                    if ( visitadosPorFogo[ vertice.first ][ vertice.second ] < visitadosPorFogo[ vertice.first+1 ][ vertice.second ] )
+                        visitadosPorFogo[ vertice.first ][ vertice.second ]++;
+                    cout << "oi4" << endl;
+                }
+            }
+            if( vertice.first-1 < R ) {
+                if( visitadosPorFogo[ vertice.first-1 ][ vertice.second ] == 0 ) {
+                    if( Labirinto[ vertice.first-1 ][ vertice.second ] == '.' || Labirinto[ vertice.first-1 ][ vertice.second ] == 'J' ) {
+                        visitadosPorFogo[ vertice.first-1 ][ vertice.second ]++;
+
+                        fila.push( make_pair( vertice.first-1 , vertice.second ) );
+                        cout << "i: " << vertice.first+-1  << " j: " << vertice.second << endl;
+                    }
+                } else { //caso seja maior que 0
+                     if ( visitadosPorFogo[ vertice.first ][ vertice.second ] < visitadosPorFogo[ vertice.first-1 ][ vertice.second ] ){
+                        visitadosPorFogo[ vertice.first ][ vertice.second ]++;
+                     }
+
+                    cout << "ooooi3" << endl;
+                }
+            }
+            if( vertice.second+1 < C ) {
+                if( visitadosPorFogo[ vertice.first  ][ vertice.second+1 ] == 0 ) {
+                    if( Labirinto[ vertice.first  ][ vertice.second+1 ] == '.' || Labirinto[ vertice.first ][ vertice.second+1 ] == 'J' ) {
+                        visitadosPorFogo[ vertice.first  ][ vertice.second+1 ]++;
+
+                        fila.push( make_pair( vertice.first  , vertice.second+1 ) );
+                        cout << "i: " << vertice.first  << " j: " << vertice.second+1 << endl;
+
+                    }
+                } else  if ( visitadosPorFogo[ vertice.first ][ vertice.second ] < visitadosPorFogo[ vertice.first ][ vertice.second+1 ] ){ //caso seja maior que 0
+                    visitadosPorFogo[ vertice.first ][ vertice.second ]++;
+
+                    cout << "ooooi2" << endl;
+                }
+            }
+            if( vertice.second-1 < C ) {
+                if( visitadosPorFogo[ vertice.first ][ vertice.second-1 ] == 0 ) {
+                    if( Labirinto[ vertice.first  ][ vertice.second-1 ] == '.' || Labirinto[ vertice.first  ][ vertice.second-1 ] == 'J' ) {
+                        visitadosPorFogo[ vertice.first  ][ vertice.second-1 ]++;
+
+                        fila.push( make_pair( vertice.first  , vertice.second-1 ) );
+                        cout << "i: " << vertice.first << " j: " << vertice.second-1 << endl;
+
+                    }
+                } else if ( visitadosPorFogo[ vertice.first ][ vertice.second ] < visitadosPorFogo[ vertice.first ][ vertice.second-1 ] ){ //caso seja maior que 0
+                    visitadosPorFogo[ vertice.first ][ vertice.second ]++;
+
+                    cout << "ooooi1" << endl;
+                }
+            }
+        // ----- ------ //    
+    }
 }
 
-void dfsFogo (int R, int C, int i, int j, vector<vector<pair<bool, int>>> &visitadosPorFogo, vector<vector<char>> &Labirinto){
+/*void bfsFogo (int R, int C, int i, int j, vector<vector<pair<bool, int>>> &visitadosPorFogo, vector<vector<char>> &Labirinto){
     if(visitadosPorFogo[i][j].first == true) return;
     visitadosPorFogo[i][j].first = true;
     visitadosPorFogo[i][j].second++;
@@ -30,6 +101,7 @@ void dfsFogo (int R, int C, int i, int j, vector<vector<pair<bool, int>>> &visit
         if(Labirinto[i+1][j] == '.' || Labirinto[i+1][j] == 'J'){
             visitadosPorFogo[i+1][j].second++;
             visitadosPorFogo[i+1][j].first = true;
+            Labirinto[i+1][j] = 'F';
             dfsFogo(R, C, i+1, j, visitadosPorFogo, Labirinto);
         }
     }
@@ -38,6 +110,7 @@ void dfsFogo (int R, int C, int i, int j, vector<vector<pair<bool, int>>> &visit
         if(Labirinto[i-1][j] == '.' || Labirinto[i-1][j] == 'J'){
             visitadosPorFogo[i-1][j].second++;
             visitadosPorFogo[i-1][j].first = true;
+            Labirinto[i-1][j] = 'F';
             dfsFogo(R, C, i-1, j, visitadosPorFogo, Labirinto);
         }
     }
@@ -46,6 +119,7 @@ void dfsFogo (int R, int C, int i, int j, vector<vector<pair<bool, int>>> &visit
         if(Labirinto[i][j+1] == '.' || Labirinto[i][j+1] == 'J'){
             visitadosPorFogo[i][j+1].second++;
             visitadosPorFogo[i][j+1].first = true;
+            Labirinto[i][j+1] = 'F';
             dfsFogo(R, C, i, j+1, visitadosPorFogo, Labirinto);
         }
     }
@@ -54,38 +128,32 @@ void dfsFogo (int R, int C, int i, int j, vector<vector<pair<bool, int>>> &visit
         if(Labirinto[i][j-1] == '.' || Labirinto[i][j-1] == 'J'){
             visitadosPorFogo[i][j-1].second++;
             visitadosPorFogo[i][j-1].first = true;
+            Labirinto[i][j-1] = 'F';
             dfsFogo(R, C, i, j-1, visitadosPorFogo, Labirinto);
         }
     }
-}
+}*/
 
 void solve(int R, int C){
     vector<vector<char>> Labirinto (R);
-    vector<vector<pair<bool,int>>> visitadosPorJoe(R, vector<pair<bool,int>> (C));
-    vector<vector<pair<bool,int>>> visitadosPorFogo(R, vector<pair<bool,int>> (C));
+    vector<vector<int>> visitadosPorJoe (R, vector<int> (C)); // confere com != 0
+    vector<vector<int>> visitadosPorFogo(R, vector<int> (C));
     
     char st;
 
-    for(int i = 0 ; i < R ; i++){
-        for(int j = 0; j < C ; j++){
+    for( int i = 0 ; i < R ; i++ ) {
+        for( int j = 0; j < C ; j++ ) {
             cin >> st;
             Labirinto[i].push_back(st);
         }
     }
 
+
+    bfsFogo( R, C, Labirinto, visitadosPorFogo );
+        
     for(int i = 0 ; i < R ; i++){
         for(int j = 0; j < C ; j++){
-            visitadosPorFogo[i][j].first = false;
-            if(Labirinto[i][j] == 'F'){
-                visitadosPorFogo[i][j].second = 0;
-                dfsFogo(R, C, i, j, visitadosPorFogo, Labirinto);
-            }
-        }
-    }
-    
-    for(int i = 0 ; i < R ; i++){
-        for(int j = 0; j < C ; j++){
-            cout << visitadosPorFogo[i][j].second;
+            cout << visitadosPorFogo[i][j];
         }    
         cout << endl;
     }
